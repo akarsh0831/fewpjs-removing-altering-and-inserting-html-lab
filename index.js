898b743f1f0f5b6dc3e854230bb8b0b13b6e9dc2
Akarsh Shukla <akarshshukla19@yahoo.com>
2021-04-02 01:31:52 +0000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main').remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+document.getElementById("victory").innerHTML = "Akarsh is the champion";// Write your code here!
